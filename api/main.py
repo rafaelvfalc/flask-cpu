253a0afb4258a5ebbcd527b266f2a39e925bcc2f
rafaelvf <rafael.falcao@ccc.ufcg.edu.br>
2018-12-28 12:32:08 -0300

    Adding post command

--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
+import os
+import subprocess
 import psutil
 
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -8,9 +11,19 @@ app = Flask(__name__)
 def home():
     return ""
 
-@app.route("/cpu")
+@app.route("/cpu", methods = ['GET', 'POST'])
 def get_cpu():
-    return str(psutil.cpu_percent()/100.0)
+    if request.method == 'GET':
+        return str(psutil.cpu_percent())
+    if request.method == 'POST':
+        cpu_quota = request.get_json(force=True)['cpu_quota']
+        id_containers = os.popen("sudo docker container ls | grep stress | awk '{print $1;}'").readlines()
+        
+        for id_container in id_containers:
+            id_container = id_container.split('\n')[0]
+            os.system("sudo docker update " + id_container + " --cpu-period 100000 --cpu-quota " + cpu_quota)
+
+        return ""
 
 if __name__ == "__main__":
     app.run(debug=True)
